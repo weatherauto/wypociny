5ea1ebb11d72f6cee9db4a08e1e8c9573da241ca
marek.miryn <jwxpauto@gmail.com>
2016-11-08 13:50:42 +0100

    mm01

--- src/main/java/Wypociny/PrintTime.java
+++ src/main/java/Wypociny/PrintTime.java
@@ -1,5 +1,7 @@
 package Wypociny;
 
+import java.util.Scanner;
+
 public class PrintTime {
 
     public static void printTime(int hour, int minute) {
@@ -8,9 +10,38 @@ public class PrintTime {
         System.out.println(minute);
     }
     public static void main(String[] args) {
-        int hour = 11;
-        int minute = 59;
-        printTime(hour +1, minute);
+        countdown(40);
     }
+
+    public static void scanDouble() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        if (!in.hasNextDouble()) {
+            String word = in.next();
+            System.err.println(word + " is not a number");
+            return;
+        }
+        double x = in.nextDouble();
+        printLogarithm(x);
+    }
+
+    private static void printLogarithm(double x) {
+        if (x <= 0.0) {
+            System.err.println("Error: x must be positive.");
+            return;
+        }
+        double result = Math.log(x);
+        System.out.println("The log of x is " + result);
+    }
+    public static void countdown(int n) {
+        if (n == 0) {
+            System.out.println("Blastoff!");
+        } else {
+
+            countdown(n - 1);
+            System.out.println(n);
+        }
+    }
+
 }
 
