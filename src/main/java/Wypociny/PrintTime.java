da4c5d3d3dd8edcfdeb14c0df05d77e577e38336
marek.miryn <jwxpauto@gmail.com>
2016-11-08 12:20:49 +0100

    mm01

---
+++ src/main/java/Wypociny/PrintTime.java
@@ -0,0 +1,16 @@
+package Wypociny;
+
+public class PrintTime {
+
+    public static void printTime(int hour, int minute) {
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.println(minute);
+    }
+    public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        printTime(hour +1, minute);
+    }
+}
+
--- src/main/java/Wypociny/Solution.java
+++ src/main/java/Wypociny/Solution.java
@@ -6,20 +6,35 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+
 public class Solution {
 
     public static void main(String[] args) {
+        double cm;
+        int feet, inches, remainder;
+        final double CM_PER_INCH = 2.54;
+        final int IN_PER_FOOT = 12;
         Scanner in = new Scanner(System.in);
-        String S = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
+// prompt the user and get the value
+        System.out.print("Exactly how many cm? ");
+        cm = in.nextDouble();
+// convert and output the result
+        inches = (int) (cm / CM_PER_INCH);
+        feet = inches / IN_PER_FOOT;
+        remainder = inches % IN_PER_FOOT;
+        System.out.printf("%.2f cm = %d ft, %d in\n",
+                cm, feet, remainder);
 
+        newLine("werwe","werwer",5);
 
-
-
-        String Ac = S.substring(start, end);
-        System.out.println(Ac);
-
-        //String Bc = B.substring(0, 1).toUpperCase() + B.substring(1);
     }
+
+    public static void newLine(String a,String b, int c) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
 }
